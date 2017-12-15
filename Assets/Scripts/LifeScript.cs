e4ab07bd2bee8c0c3bb659021e16bae12e4c54cc
anthonyaitlhadj <aaitlhadj@MacBook-Pro-de-Anthony.local>
2017-12-15 15:56:58 +0100

    Modification Terrain et Creation des prefabs ennemis

---
+++ Assets/Scripts/LifeScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeScript : MonoBehaviour {
+
+	public int Pv; 
+
+	public virtual void Damage(int d)
+	{
+		Pv -= d;
+	}
+}
---
+++ Assets/Scripts/MvtEnnemis.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MvtEnnemis : LifeScript {
+
+	private NavMeshAgent agent;
+	public Transform Destination;
+	private Animator animator;
+
+	void Start(){
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	public override void Damage(int d)
+	{
+		base.Damage(d);
+		GetComponent<Animator>().SetInteger("Pv", Pv);
+		if (Pv <= 0) 
+		{
+			GetComponent<BoxCollider>().enabled = false;
+			gameObject.tag = "Untagged";
+			Destroy (this);
+		}
+	}
+
+	void Update(){
+		agent.destination = Destination.position;
+	}
+}
