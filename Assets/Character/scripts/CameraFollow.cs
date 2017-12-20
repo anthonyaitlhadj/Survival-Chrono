34d67b0e1a909b40cd0cb9b8572c5bbbd458bca5
Grégoire Joncour <greg.joncour@gmail.com>
2017-12-21 00:34:36 +0100

    finalisation camera

--- Assets/Character/scripts/CameraFollow.cs
+++ Assets/Character/scripts/CameraFollow.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
     public GameObject player;
     float currentHeight;
-
     // Use this for initialization
     void Start () 
-    {
+    {currentHeight = player.transform.position.y;
     }
     
     void LateUpdate () 
@@ -20,12 +19,12 @@ public class CameraFollow : MonoBehaviour {
         //float XClamp = Mathf.Clamp(CamTransform.eulerAngles.x + x, 60, -60);
         //player.transform.eulerAngles += new Vector3(0, y, 0) * 1;
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        currentHeight = player.transform.position.y;
-
+        
         Vector3 velocity = Vector3.zero;
-		Vector3 forward = player.transform.forward;
-		Vector3 needPos = player.transform.position - forward ;
-		transform.position = Vector3.SmoothDamp(new Vector3 (transform.position.x, currentHeight, transform.position.z), needPos,ref velocity,0.05f);
+		Vector3 needPos = player.transform.position - player.transform.forward * 1.5f ;
+        needPos.y = currentHeight -= x * Time.deltaTime;
+		transform.position = Vector3.SmoothDamp(new Vector3 (transform.position.x, currentHeight-=x* Time.deltaTime, transform.position.z), needPos ,ref velocity,0.05f);
+        
 		transform.LookAt(player.transform);
 		//transform.rotation = player.transform.rotation;
     }
