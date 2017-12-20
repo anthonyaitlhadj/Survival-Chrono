b53354ab61f8f25c9430339c9488dbc984f1e470
Grégoire Joncour <greg.joncour@gmail.com>
2017-12-21 00:39:44 +0100

    camera

--- Assets/Character/scripts/CameraFollow.cs
+++ Assets/Character/scripts/CameraFollow.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
-    
+
     public GameObject player;
     float currentHeight;
 
@@ -16,9 +16,10 @@ public class CameraFollow : MonoBehaviour {
     {
         float x = Input.GetAxis("Mouse Y");
         Vector3 velocity = Vector3.zero;
-		Vector3 needPos = player.transform.position - player.transform.forward * 1.5f ;
-        needPos.y = currentHeight -= x * Time.deltaTime;
-		transform.position = Vector3.SmoothDamp(new Vector3 (transform.position.x, currentHeight-=x* Time.deltaTime, transform.position.z), needPos ,ref velocity,0.05f);
+		Vector3 targetPos = player.transform.position - player.transform.forward * 1.5f ;
+        Vector3 currentPos = transform.position;
+        targetPos.y = currentPos.y = currentHeight -= x * Time.deltaTime;
+		transform.position = Vector3.SmoothDamp(currentPos, targetPos ,ref velocity,0.05f);
 		transform.LookAt(player.transform);
     }
 
