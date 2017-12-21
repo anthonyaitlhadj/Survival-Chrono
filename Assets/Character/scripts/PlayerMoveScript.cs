b08faf73b535228da806d48cfa6091ae2a82cd3b
Grégoire Joncour <greg.joncour@gmail.com>
2017-12-21 11:30:49 +0100

    New FPS character

--- Assets/Character/scripts/PlayerMoveScript.cs
+++ Assets/Character/scripts/PlayerMoveScript.cs
@@ -4,52 +4,103 @@ using UnityEngine;
 
 public class PlayerMoveScript : MonoBehaviour {
 
-    public float SpeedMovement;
-    public float JumpPower;
-    public bool CanJump = false;
-    public float MaxSpeed;
+    public float m_moveSpeed = 2;
+    public float m_turnSpeed = 200;
+    public float m_jumpForce = 4;
+    public Rigidbody m_rigidBody;
+    public Transform CamTransform;
+
+    private float m_currentV = 0;
+    private float m_currentH = 0;
+    private readonly float m_interpolation = 10;
+
+    private bool m_wasGrounded;
+    private Vector3 m_currentDirection = Vector3.zero;
+    private float m_jumpTimeStamp = 0;
+    private float m_minJumpInterval = 0.25f;
+    private bool m_isGrounded;
+    private List<Collider> m_collisions = new List<Collider>();
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
 
-        if(GetComponent<Rigidbody>().velocity.magnitude < MaxSpeed)
-        {
-            if (Input.GetKey(KeyCode.Z))
-            {
-                //GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, SpeedMovement));
-                GetComponent<Rigidbody>().AddForce(transform.forward * SpeedMovement);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                //GetComponent<Rigidbody>().AddForce(new Vector3(SpeedMovement, 0, 0));
-                GetComponent<Rigidbody>().AddForce(transform.right * SpeedMovement);
-            }
-            if (Input.GetKey(KeyCode.Q))
-            {
-                //GetComponent<Rigidbody>().AddForce(new Vector3(-SpeedMovement, 0, 0));
-                GetComponent<Rigidbody>().AddForce(-transform.right * SpeedMovement);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                //GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -SpeedMovement));
-                GetComponent<Rigidbody>().AddForce(-transform.forward * SpeedMovement);
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && CanJump)
-        {
-            GetComponent<Rigidbody>().AddForce(new Vector3(0, JumpPower, 0));
-        }
-        CanJump = false;
+    void Update(){
+        MoveUpdate();
+        //m_wasGrounded = m_isGrounded;
     }
 
-    private void OnTriggerStay(Collider other)
+    private void MoveUpdate()
     {
-        CanJump = true;
+        float mouse_y = Input.GetAxis("Mouse X");
+        float mouse_x = Input.GetAxis("Mouse Y");
+         transform.eulerAngles += new Vector3(0, mouse_y, 0) * m_moveSpeed/2;
+        CamTransform.eulerAngles += new Vector3(-mouse_x, 0, 0) * m_moveSpeed/2;
+
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        bool walk = Input.GetKey(KeyCode.LeftShift);
+
+        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
+        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
+
+        // devant derrière
+        transform.position += transform.forward * m_currentV * m_moveSpeed * Time.deltaTime;
+        // gauche droite
+        transform.position += transform.right * m_currentH * m_moveSpeed * Time.deltaTime;
+        //transform.Rotate(0, m_currentH * m_turnSpeed * Time.deltaTime, 0);
+
+        JumpingAndLanding();
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        ContactPoint[] contactPoints = collision.contacts;
+        bool validSurfaceNormal = false;
+        for (int i = 0; i < contactPoints.Length; i++)
+        {
+            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
+            {
+                validSurfaceNormal = true; break;
+            }
+        }
+
+        if(validSurfaceNormal)
+        {
+            m_isGrounded = true;
+            if (!m_collisions.Contains(collision.collider))
+            {
+                m_collisions.Add(collision.collider);
+            }
+        } else
+        {
+            if (m_collisions.Contains(collision.collider))
+            {
+                m_collisions.Remove(collision.collider);
+            }
+            if (m_collisions.Count == 0) { m_isGrounded = false; }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(m_collisions.Contains(collision.collider))
+        {
+            m_collisions.Remove(collision.collider);
+        }
+        if (m_collisions.Count == 0) { m_isGrounded = false; }
+    }
+
+    private void JumpingAndLanding()
+    {
+        bool jumpCooldownOver = (Time.time - m_jumpTimeStamp) >= m_minJumpInterval;
+
+        if (jumpCooldownOver && m_isGrounded && Input.GetKey(KeyCode.Space))
+        {
+            m_jumpTimeStamp = Time.time;
+            m_rigidBody.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse);
+        }
     }
 }
--- Assets/Character/scripts/PlayerRotationScript.cs
+++ Assets/Character/scripts/PlayerRotationScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerRotationScript : MonoBehaviour {
-    //public Transform CamTransform;
+    public Transform CamTransform;
     public float SpeedRotation;
 
     // Use this for initialization
@@ -14,11 +14,10 @@ public class PlayerRotationScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float y = Input.GetAxis("Mouse X");
-        //float x = Input.GetAxis("Mouse Y");
+        float x = Input.GetAxis("Mouse Y");
         //Debug.Log(x + " " + y);
         transform.eulerAngles += new Vector3(0, y, 0) * SpeedRotation;
         //float XClamp = Mathf.Clamp(CamTransform.eulerAngles.x + x, 60, -60);
-        //CamTransform.eulerAngles += new Vector3(-x, 0, 0) * SpeedRotation;
+        CamTransform.eulerAngles += new Vector3(-x, 0, 0) * SpeedRotation;
     }
 }
-
