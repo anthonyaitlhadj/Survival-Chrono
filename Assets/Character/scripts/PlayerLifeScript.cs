213307c5020f6729bb38aeef3c39cc487fbfe645
Grégoire Joncour <greg.joncour@gmail.com>
2017-12-22 00:07:01 +0100

    reload scene

---
+++ Assets/Character/scripts/PlayerLifeScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLifeScript : LifeScript
+{
+	public override void Damage(int d){
+		base.Damage(d);
+		if(Pv <= 0){
+			SceneManager.LoadScene("SalleGame");
+		}
+	}
+
+}
